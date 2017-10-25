9191ecd5333fa3ded93cc3fcd18b2cdfea7b66f5
Aruna Bolisetti <ruby.aruna28@gmail.com>
2017-10-25 11:23:47 -0400

    changes made

--- src/test/java/com/bank/tests/HomePageIT.java
+++ src/test/java/com/bank/tests/HomePageIT.java
@@ -2,15 +2,16 @@ package com.bank.tests;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import PageObjects.EnrollPO;
 import PageObjects.HomePagePO;
 
 public class HomePageIT extends BaseIT {
@@ -18,25 +19,37 @@ public class HomePageIT extends BaseIT {
 	private String baseUrl;
 	private StringBuffer verificationErrors = new StringBuffer();
 	
-	@BeforeClass(alwaysRun = true)
+	@BeforeMethod(alwaysRun = true)
 	public void SetUp() throws Exception {
 		driver = new ChromeDriver();
-		//driver = new RemoteWebDriver(new URL("http://35.193.227.180:4444/wd/hub"),DesiredCapabilities.chrome());
 		baseUrl = "https://www.wellsfargo.com/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
-	@Test
+	@Test(priority = 1)
 	public void testWellsFargo() throws Exception {
 		// preconditions
 		driver.get(baseUrl);
+		//Search for jobs
 		HomePagePO home = new HomePagePO(driver);
 		home.searchTextfield.sendKeys("jobs wells fargo");
-		home.searchButton.click();
-		
-		
-		
-		//home.enrollLink.click();
+		home.searchButton.click();	
+		//Job search results displayed 
+		// verify search results title
+		WebElement search = driver.findElement(By.id("skip"));
+		Assert.assertEquals(true, search.isDisplayed());
+		System.out.println("Search results displayed");
+	
+	}
+	
+	@Test(priority=2)
+	public void testEnroll() throws Exception {
+		 driver.get(baseUrl);
+	   //click on enroll link
+	   HomePagePO home1 = new HomePagePO(driver);
+	   home1.enrollLink.click();
+   }
+	
 		
 //		EnrollPO enroll = new EnrollPO(driver);
 //		enroll.continueButton.click();
@@ -45,12 +58,26 @@ public class HomePageIT extends BaseIT {
 //	    enroll.wellsFargologo.click();
 //		System.out.println("Logo link is clicked");
 //		this.screenshot(driver,"home", "home_link.png");
+	
+	
+	@Test(priority=3)
+	public void testCareers() throws Exception {
+		//open browser
+		driver.get(baseUrl);
+		HomePagePO home = new HomePagePO(driver);
+		home.careersLink.click();
+		//Verify page title is present
+		WebElement careers = driver.findElement(By.id("skip"));
+		Assert.assertEquals(true, careers.isDisplayed());
+		System.out.println("careers title is present");
+	
+		
 	}
 	
 		@AfterMethod
 		@AfterClass(alwaysRun = true)
 		public void tearDown() throws Exception {
-			driver.quit();
+			driver.close();
 			String verificationErrorString = verificationErrors.toString();
 			if (!"".equals(verificationErrorString)) {
 				Assert.fail(verificationErrorString);
